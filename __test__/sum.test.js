31df897aad2b1d85fbf9a0fb9f9aecb646ed4d9d
Sandra Chung <sandra@moveon.org>
2017-08-01 16:40:03 -0600

    a little more sleuthing on babel-jest errors

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,5 +1,13 @@
-const sum = require('./sum');
+// const sum = require('./sum');
+import { sum } from './sum';
+import { graphql } from 'graphql';
+import { schema, resolvers } from '../src/server/api/schema'; // comment out this line and the test should run normally - this suggests that we're failing to transpile spoke project code, but transpiling works on test code and node_modules
 
 test('adds 1+2 to equal 3', () => {
   expect(sum(1,2)).toBe(3);
+});
+
+test('logs something', () => {
+  console.log('test cache?');
+  expect(1).toBeTruthy;
 });
