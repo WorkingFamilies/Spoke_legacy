ff9de8b2d0e1e33e17799b020b0def972be05ea5
Ann Lewis <the.annlewis@gmail.com>
2017-08-02 09:22:43 -0700

    first not yet working draft of a custom jest transformer starting from http://facebook.github.io/jest/docs/tutorial-react.html#custom-transformers . next steps: figure out why any issues with process fail silently and how to get the output, then have the transpiler transpile the local model files first

--- dev-tools/jest.transform.js
+++ dev-tools/jest.transform.js
@@ -1,6 +1,20 @@
-#!/usr/bin/env node
-var config = require('dotenv').config()
-require('babel-register')
-require('babel-polyfill')
-require('../' + process.argv[2])
-model.exports = require('babel-jest').createTransformer(config)
+// custom-transformer.js
+'use strict';
+console.log("**** in jest.transform.js !");
+
+const babel = require('babel-core');
+const jestPreset = require('babel-preset-jest');
+
+module.exports = {
+  process(src, filename) {
+    console.log("**** processing file: ", filename);
+    if (babel.util.canCompile(filename)) {
+      return babel.transform(src, {
+        filename,
+        presets: [jestPreset],
+        retainLines: true,
+      }).code;
+    }
+    return src;
+  },
+};
