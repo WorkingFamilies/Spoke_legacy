373c1c67d29e1e4a540142cf10230fadb77c2975
Sandra Chung <sandra@moveon.org>
2017-08-02 11:14:47 -0600

    merge
     On branch knex-jest

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,7 +1,7 @@
 // const sum = require('./sum');
 import { sum } from './sum';
 import { graphql } from 'graphql';
-import { schema, resolvers } from '../src/server/api/schema'; // comment out this line and the test should run normally - this suggests that we're failing to transpile spoke project code, but transpiling works on test code and node_modules
+// import { schema, resolvers } from '../src/server/api/schema'; // comment out this line and the test should run normally - this suggests that we're failing to transpile spoke project code, but transpiling works on test code and node_modules
 
 test('adds 1+2 to equal 3', () => {
   expect(sum(1,2)).toBe(3);
